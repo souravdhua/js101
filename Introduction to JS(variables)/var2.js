1d844bc3fca6f534a8c16cafc056a49dc0cecb9e
Sourav Dhua <souravdhua1997@gmail.com>
2023-02-08 00:15:53 +0530

    addadJS101

---
+++ Introduction to JS(variables)/var2.js
@@ -0,0 +1,6 @@
+let name="Sourav Dhua"
+console.log("My name is ",name);
+name="Banamali Dhua"
+console.log("the name of my father is ",name);
+name="Krishna Dhua"
+console.log("The name of my mother is ",name);
---
+++ Introduction to JS(variables)/var3.js
@@ -0,0 +1,5 @@
+my_name="Sourav Dhua"
+my_age=25
+
+console.log(typeof(my_name))
+console.log(typeof(my_age))
